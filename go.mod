48f1439a75cb0bfccf55ec158fe6e40b8866d933
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2021-11-23 12:45:50 -0800

    add inmemory graph storage

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/igavrysh/linksrus
 
 go 1.17
+
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/kr/text v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
---
+++ graph/store/memory_test.go
@@ -0,0 +1,20 @@
+package store
+
+import (
+	"github.com/igavrysh/linksrus/graph/graphtest"
+	gc "gopkg.in/check.v1"
+	"testing"
+)
+
+var _ = gc.Suite(new(InMemoryGraphTestSuite))
+
+func Test(t *testing.T) { gc.TestingT(t) }
+
+type InMemoryGraphTestSuite struct {
+	graphtest.SuiteBase
+}
+
+func (s *InMemoryGraphTestSuite) SetUpTest(c *gc.C) {
+	s.SetGraph(NewInMemoryGraph())
+}
+
